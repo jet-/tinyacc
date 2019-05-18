e01641351769dc73a147216ba242e106a472d183
root <root@server.itssmb.com>
2019-05-18 19:51:25 -0400

    Revamping Balance by Groups report

--- rep1_groups.php
+++ rep1_groups.php
@@ -1,3 +1,30 @@
+<head>
+
+<style>
+.collapsible {
+  background-color: #F6F6F6;
+  color: black;
+  cursor: pointer;
+  padding: 1px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active, .collapsible:hover {
+  background-color: #F6F6F6;
+}
+
+.content {
+  padding: 0 18px;
+  display: none;
+  overflow: hidden;
+  background-color: #F6F6F6;
+}
+</style>
+</head>
 <?php
 
 require_once("conf.php");
@@ -8,26 +35,27 @@ require_once("menu.php");
 <br><br><br>
 <form name="form" action="<?php echo $PHP_SELF;?>" method="post" enctype="multipart/form-data">
 <fieldset>
-<legend>Report by Groups</legend>
+<legend>Report</legend>
 
     <br> <b> From date</b>: <input type="text" name="from" value= "<?php echo date("Y-m")."-01"; ?>" size=10 maxlength=10 style="background: #FFFFCC;"> &nbsp;&nbsp;&nbsp;
     <b> To date</b>:  <input type="text"     name="to"      value= "<?php echo date("Y-m-d"); ?>"size=10 maxlength=10  style="background: #FFFFCC;" >
 	              <input type="checkbox" name="full_ov" value="yes" > Balance for the period &nbsp;&nbsp;
-	              <input type="checkbox" name="show_ledger" value="no" checked > Show Ledger &nbsp;&nbsp;
+	              <input type="checkbox" name="show_ledger" value="yes" unchecked > Show Ledger &nbsp;&nbsp;
 	              <input type="checkbox" name="show_balance" value="yes" checked> Show Balance &nbsp;&nbsp;
 		      <input type="submit"   name="send"    value="Generate" autofocus >
 <br><br>
 </fieldset>
 </form>
 <?php
-$query = ("
+$query ="
 	  SELECT ledger.id AS id,  t1.name AS name_dt, ledger.ammount, t2.name AS name_ct, date, time, created, accounted, text
 	  FROM items t1, items t2, ledger
 	  WHERE t1.id=ledger.item_dt AND t2.id=ledger.item_ct AND ledger.date>=\"". $_POST['from']."\" AND ledger.date<=\"". $_POST['to']."\" 
-	  ORDER BY ledger.date desc,ledger.id desc;");
+	  ORDER BY ledger.date desc,ledger.id desc;";
 
 $result = $mysqli->query($query);
 $rowCount = mysqli_num_rows($result);
+
 
 if ($_POST['show_ledger'] == "yes" and $rowCount > 0 ) { 
 
@@ -35,7 +63,7 @@ if ($_POST['show_ledger'] == "yes" and $rowCount > 0 ) {
 	echo "<caption> GENERAL LEDGER &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;from: {$_POST['from']} to: {$_POST['to']}";
 	echo "&nbsp;&nbsp;&nbsp;&nbsp; $rowCount rows </caption> ";
 	$i=1;
-	if ($result = $mysqli->query($query)) {
+	if ($result = $mysqli->query($query) ) {
 
 	echo "<thead><tr> 
 		<th> # </th> 
@@ -49,7 +77,7 @@ if ($_POST['show_ledger'] == "yes" and $rowCount > 0 ) {
 		<th width=25 >Last Modified</th> 
 	      </tr></thead>";
 
-	  while($row = $result->fetch_assoc()) {
+	  while($row = $result->fetch_assoc() ) {
 		echo "<tr>";
 		if ($i%2 ==0 ) {
 			 echo '<tr style="background: #eeeeee;" >';
@@ -84,178 +112,202 @@ if ($_POST['show_ledger'] == "yes" and $rowCount > 0 ) {
 
 if ($_POST['show_balance'] == "yes" ) { 
 
-	#ov
-	#type_: L - razhod; A - prihod
-	$result = $mysqli->query("SELECT  items.id, items.acc_group, acc_groups.name as name, type, liquidity, orderby FROM items, acc_groups Where items.acc_group=acc_groups.acc_group Group by acc_group ORDER by acc_group ");
-	$counter=1;
-	while ($row0 = $result->fetch_assoc() ) {
-	    $data[$counter][1]= $row0['name'];
-	    $data[$counter][4]= $row0['type'];
-	    $data[$counter][7]= $row0['liquidity'];
-	    $data[$counter][8]= $row0['orderby'];
-	    $data[$counter][11]= $row0['acc_group'];
-	    $counter++;
-	}
-
 	if ($_POST['full_ov'] <> "yes" ) { 
 	  $_POST['from']="2000-01-01";
 	}
 
 
-	#filling $data[$i][2] credit turnover
-	$result = $mysqli->query("
-	    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
-	    FROM items
-	    LEFT JOIN  ledger ON ledger.item_dt=items.id and ledger.date<=\"" . $_POST['to'] . "\" 
-		      and ledger.date>=\"" . $_POST['from'] . "\" and accounted 
-	    GROUP BY items.acc_group
-	    ORDER BY acc_group ");
-
-
-	$counter=1;
-	while ($row1 = $result->fetch_assoc() ) {
-	    $data[$counter][2]= $row1['amnt'];
-	    $counter++;
-	}
-
-
-
-	#filling $data[$i][3] debit turnover
-	$result = $mysqli->query("
-	    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
-	    FROM items
-	    LEFT JOIN  ledger ON ledger.item_ct=items.id and ledger.date<=\"" . $_POST['to'] . "\" 
-		      and ledger.date>=\"" . $_POST['from'] . "\"  and  accounted 
-	    GROUP BY items.acc_group
-	    ORDER BY acc_group ");
-
-	$counter=1;
-	while ($row2 = $result->fetch_assoc() ) {
-	    $data[$counter][3]= $row2['amnt'];
-	    $counter++;
-	}
-
-
-
-
-	#filling $data[$i][5] yearly debit turnover
-	$result = $mysqli->query("
-	    SELECT  sum(ledger.ammount) AS amnt
-	    FROM items
-	    LEFT JOIN ledger on ledger.item_dt=items.id and YEAR(ledger.date)=\"" . date("Y",strtotime($_POST['to'])) . "\"  and accounted 
-	    GROUP BY items.acc_group
-	    ORDER BY acc_group ");
-
-	$counter=1;
-	while ($row1 = $result->fetch_assoc() ) {
-	    $data[$counter][5]= $row1['amnt'];
-	    $counter++;
-	}
-
-
-	#filling $data[$i][6] yearly credit turnover
-	$result = $mysqli->query("
-	      SELECT  sum(ledger.ammount) as amnt
-	      FROM items 
-	      LEFT JOIN  ledger on ledger.item_ct=items.id and YEAR(ledger.date)=\"" . date("Y",strtotime($_POST['to'])) . "\"  and accounted
-	      GROUP BY items.acc_group
-	      ORDER BY orderby ");
-
-
-	$counter=1;
-	while ($row1 = $result->fetch_assoc() ) {
-	    $data[$counter][6]= $row1['amnt'];
-	    $counter++;
-	}
-
-	# $data[][1] - item name
-	#	 [2] - total turnover DT 
-	#	 [3] - total turnover CT
-	#	 [4] - item type Asset / Liability
-	#	 [5] - yearly turnover DT
-	#	 [6] - yearly turnover CT
-	#	 [7] - liquidity +/-
-	#	 [8] - orderby
-	#	[11] - account id
 
 	echo "<table  class=\"ref\"  bgcolor=\"#DDD4FF\">";
 	echo "<caption> BALANCE SHEET &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; from: ". $_POST['from'] . "&nbsp;&nbsp; to: ". $_POST['to'];
 	echo "</caption>   
-			<tr>   
-				<th> Item </th>  
-				<th> Ammount DT </th>  
-				<th> Ammount CT </th> 
-				<th> Ammount DT<br> annual </th>
-				<th> Ammount CT <br>annual </th>
-				<th> Ammount </th>   
-				<th>liab.</th>  
-				<th>asset</th>   
-				<th></th> 
-				<th></th> 
-			</tr>";
+			";
 
-	$prihod=0;
-	$razhod=0;
-	for ($i=1;$i<$counter;$i++) {
-		if ($data[$i][4] == "L" ) { 
-		    $razhod += ($data[$i][3] - $data[$i][2]);
-		}
+	echo '<tr>
+		<th width="219"> Item </th>
+		<th width="80">Ammount DT</th>
+		<th width="81">Ammount CT</th>
+		<th width="81">Ammount DT<br> annual </th>
+		<th width="81">Ammount CT <br>annual</th>
+		<th style="background: #eeeeee;"  width="81">Ammount </th> </tr>
+		</table>';
 
-		if ($data[$i][4] == "A" ) { 
-		    $prihod += ($data[$i][3] - $data[$i][2]);
-		}
-	}
 
-	$liq=0;
+	$result = $mysqli->query("SELECT  name,acc_group  FROM  acc_groups ");
+	while ($groups = $result->fetch_assoc() ) {
+	    $group_name = $groups['name'];
+	    $group_id   = $groups['acc_group'];
 
-	for ($i=1;$i<$counter;$i++) {
-	    if ($data[$i][2] <> 0 or $data[$i][3] <> 0) {
-		echo "<tr>";
-		  echo "<td> <a href=\"rep2.php?account=" . number_format($data[$i][11],0) . "&curr=" .   $_GET['curr'] . "\">". $data[$i][1] . " </a></td>";
-		  echo "<td align=\"right\">" . number_format($data[$i][2],2) . "</td>";
-		  echo "<td align=\"right\">" . number_format($data[$i][3],2) .	"</td>";
-		  echo "<td align=\"right\">" . number_format($data[$i][5],2) .	"</td>";
-		  echo "<td align=\"right\">" . number_format($data[$i][6],2) .	"</td>";
-		  echo "<td align=\"right\"  style=\"background: #eeeeee;\" >" . number_format($data[$i][3] - $data[$i][2],2) . " </td> ";
+	    $result1 = $mysqli->query("
+		    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+		    FROM items
+		    LEFT JOIN  ledger ON ledger.item_dt=items.id 
+		    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+			   and ledger.date>=\"" . $_POST['from'] . "\" 
+			   and accounted 
+			   and acc_group = $group_id
+		    GROUP BY items.acc_group
+		    ORDER BY acc_group ");
 
-		if ($data[$i][4] == "L" ) { 
-		  echo "<td align=\"right\">" .  number_format(-100 * $data[$i][2]/$razhod,0) . "%</td>";
-		  echo "<td align=\"center\"> - </td>";
-		}
+	    $row = $result1->fetch_assoc()  ;
+	    $group_dt_turn = $row['amnt'];
+	    
+	    $result1 = $mysqli->query("
+		    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+		    FROM items
+		    LEFT JOIN  ledger ON ledger.item_ct=items.id 
+		    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+			   and ledger.date>=\"" . $_POST['from'] . "\" 
+			   and accounted 
+			   and acc_group = $group_id
+		    GROUP BY items.acc_group
+		    ORDER BY acc_group ");
 
-		if ($data[$i][4] == "A" ) { 
-		  echo "<td align=\"center\"> - </td>";
-		  echo "<td align=\"right\">" .  number_format(100 * $data[$i][3]/$prihod,0 ) . "%</td>";
-		}
-		if ($data[$i][4] == "" ) { 
-		  echo "<td align=\"center\"> - </td>";
-		  echo "<td align=\"center\"> - </td>";
-		}
-	 	  echo "<td align=\"center\"  style=\"border:0px solid black\"> " .  ($data[$i][7] ) . " </td>";
-		  echo "<td align=\"center\"  style=\"border:0px solid black\"> " .  ($data[$i][4] ) . " </td>";
-	       echo "</tr>";
+	    $row = $result1->fetch_assoc()  ;
+	    $group_ct_turn = $row['amnt'];
+
+
+	    $result1 = $mysqli->query("
+		    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+		    FROM items
+		    LEFT JOIN  ledger ON ledger.item_dt=items.id 
+		    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+			   and YEAR(ledger.date) =\"" . date("Y",strtotime($_POST['to'])) . "\" 
+			   and accounted 
+			   and acc_group = $group_id
+		    GROUP BY items.acc_group
+		    ORDER BY acc_group ");
+
+	    $row = $result1->fetch_assoc()  ;
+	    $group_dt_turn_y = $row['amnt'];
+
+	    $result1 = $mysqli->query("
+		    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+		    FROM items
+		    LEFT JOIN  ledger ON ledger.item_ct=items.id 
+		    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+			   and YEAR(ledger.date) =\"" . date("Y",strtotime($_POST['to'])) . "\" 
+			   and accounted 
+			   and acc_group = $group_id
+		    GROUP BY items.acc_group
+		    ORDER BY acc_group ");
+
+	    $row = $result1->fetch_assoc()  ;
+	    $group_ct_turn_y = $row['amnt'];
+
+	    if ( $group_dt_turn <> 0 or $group_ct_turn <>0 ) {
+	    echo "<button class=\"collapsible\">
+		<table   class=\"ref\"  bgcolor=\"#cae6ff\">
+		<tr>
+		<td  width=\"215\"> 🢒 $group_name </td>
+		<td align=\"right\" width=\"80\">" . number_format($group_dt_turn,2) . "</td>
+		<td align=\"right\" width=\"80\">" . number_format($group_ct_turn,2) . "</td>
+		<td align=\"right\" width=\"80\">" . number_format($group_dt_turn_y,2) . "</td>
+		<td align=\"right\" width=\"80\">" . number_format($group_ct_turn_y,2) . "</td>
+		<td align=\"right\" width=\"80\">" . number_format($group_ct_turn - $group_dt_turn,2) . "</td> 		</tr>
+		</table>";
 	    }
-		if ($data[$i][7] == "-" ) { 
-		    $liq -= ($data[$i][3] - $data[$i][2]);
+		echo "</button>
+			<div class=\"content\">
+			  <table   class=\"ref\"  bgcolor=\"#DDD4FF\">";
+
+		$result1 = $mysqli->query("SELECT  id, name, acc_group  FROM  items WHERE acc_group = $group_id");
+		while ($groups = $result1->fetch_assoc() ) {
+		    $acc_name = $groups['name'];
+		    $acc_id   = $groups['id'];
+
+
+
+		    $result2 = $mysqli->query("
+			    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+			    FROM items
+			    LEFT JOIN  ledger ON ledger.item_dt=items.id 
+			    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+				   and ledger.date>=\"" . $_POST['from'] . "\" 
+				   and accounted 
+				   and item_dt = $acc_id
+			    GROUP BY items.acc_group
+			    ORDER BY acc_group ");
+
+		    $row = $result2->fetch_assoc()  ;
+		    $acc_dt_turn = $row['amnt'];
+
+		    $result2 = $mysqli->query("
+			    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+			    FROM items
+			    LEFT JOIN  ledger ON ledger.item_ct=items.id 
+			    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+				   and ledger.date>=\"" . $_POST['from'] . "\" 
+				   and accounted 
+				   and item_ct = $acc_id
+			    GROUP BY items.acc_group
+			    ORDER BY acc_group ");
+
+		    $row = $result2->fetch_assoc()  ;
+		    $acc_ct_turn = $row['amnt'];
+
+
+		    $result2 = $mysqli->query("
+			    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+			    FROM items
+			    LEFT JOIN  ledger ON ledger.item_dt=items.id 
+			    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+				   and YEAR(ledger.date)=\"" . date("Y",strtotime($_POST['to'])) . "\" 
+				   and accounted 
+				   and item_dt = $acc_id
+			    GROUP BY items.acc_group
+			    ORDER BY acc_group ");
+
+		    $row = $result2->fetch_assoc()  ;
+		    $acc_dt_turn_y = $row['amnt'];
+
+		    $result2 = $mysqli->query("
+			    SELECT items.name, items.acc_group AS name, sum(ledger.ammount) AS amnt
+			    FROM items
+			    LEFT JOIN  ledger ON ledger.item_ct=items.id 
+			    WHERE  ledger.date<=\"" . $_POST['to'] . "\" 
+				   and YEAR(ledger.date)=\"" . date("Y",strtotime($_POST['to'])) . "\" 
+				   and accounted 
+				   and item_ct = $acc_id
+			    GROUP BY items.acc_group
+			    ORDER BY acc_group ");
+
+		    $row = $result2->fetch_assoc()  ;
+		    $acc_ct_turn_y = $row['amnt'];
+
+
+		    echo "<tr><td  width=\"200\"> $acc_name </td>
+			<td align=\"right\" width=\"80\">" . number_format($acc_dt_turn,2) . "</td>
+			<td align=\"right\" width=\"80\">" . number_format($acc_ct_turn,2) . "</td>
+			<td align=\"right\" width=\"80\">" . number_format($acc_dt_turn_y,2) . "</td>
+			<td align=\"right\" width=\"80\">" . number_format($acc_ct_turn_y,2) . "</td>
+			<td align=\"right\"  style=\"background: #eeeeee;\"  width=\"80\">" . number_format($acc_ct_turn - $acc_dt_turn,2) . "</td> </tr>";
+		    
 		}
 
-		if ($data[$i][7] == "+" ) { 
-		    $liq += (-1)*($data[$i][3] - $data[$i][2]);
-		}
+		echo "</table>
+			</div>";
+
 	}
-
-	echo "</table>";
-
-	echo "<br> <pre>";
-	echo " Revenue:  " 	. number_format($prihod,2) 				. "<br>";
-	echo "Expenses: " 	. number_format($razhod,2) 				. "<br>";
-	echo "  Equity: " 	. number_format($prihod + $razhod,2) 			. "<br>";
-	echo "<hr>";
-	echo "  Profit: " 	. number_format($liq,2) 				. "<br></pre>";
 }
 
-mysql_close();
+$mysqli->close();
 ?>
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
 
 
