28a03e34e13cc1c82ed2b42a018eece92c577920
root <root@server.itssmb.com>
2021-05-08 10:59:07 -0400

    fixes

--- conf.php
+++ conf.php
@@ -1,17 +1,20 @@
 <?php
 
 $curr = $_GET['curr'];
+if (empty($_GET['curr'])) {
+    $curr='usd';
+}
+
+
+// Fill these variables
 $hostName = "localhost";
 $userName = "tinyacc";
 $password = "password";
 $dbName = "acc_" . $curr;
-
-// make connection to database
-// mysqli
-$mysqli = new mysqli($hostName, $userName, $password, $dbName) or die("Unable to connect to host $hostName");
-
-
-
 date_default_timezone_set("America/New_York");
+// till here
 
+
+
+$mysqli = new mysqli($hostName, $userName, $password, $dbName) or die("Unable to connect to host $hostName");
 ?>
