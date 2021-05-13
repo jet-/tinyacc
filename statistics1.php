a90e4c47bd84c3a80d4122959085733f75e22fa4
root <root@server.itssmb.com>
2021-05-12 21:19:05 -0400

    adding charts to statistics

--- statistics1.php
+++ statistics1.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 require_once("conf.php");
 require_once("menu.php");
@@ -27,6 +27,7 @@ $tables_heather ="
 echo $tables_heather;
 echo 	"<caption> Monthly Profit </caption>"; 
 
+$counter=1;
 for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 	echo "<tr> <td><b> $yyyy </b></td>";
 	$total = 0;
@@ -38,7 +39,10 @@ for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 		echo "<td align=\"right\"> " . number_format($profit,0) . "</td>";
         }
 	echo "<td align=\"right\"><b>" . number_format($total,2) . "</b></td></tr>";
-}
+	$data[$counter][1]= "\"" . $yyyy . "\"";
+	$data[$counter][4]= number_format($total   ,0,".","");
+	$counter++;
+} 
 echo "</table>";
 
 
@@ -46,6 +50,7 @@ echo "</table>";
 echo $tables_heather;
 echo 	"<caption> Monthly Revenue </caption>"; 
 
+$counter=1;
 for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 	echo "<tr> <td><b> $yyyy </b></td>";
 	$total = 0;
@@ -56,13 +61,15 @@ for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 		echo "<td align=\"right\"> " . number_format($revenue,0) . "</td>";
         }
 	echo "<td align=\"right\"><b>" . number_format($total,2) . "</b></td></tr>";
+	$data[$counter][2]= number_format($total   ,0,".","");
+	$counter++;
 }
 echo "</table>";
-
 
 echo $tables_heather;
 echo 	"<caption> Monthly Expenses </caption>"; 
 
+$counter=1;
 for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 	echo "<tr> <td><b> $yyyy </b></td>";
 	$total = 0;
@@ -73,6 +80,8 @@ for ($yyyy=2006;$yyyy<2030;$yyyy++) {
 		echo "<td align=\"right\"> " . number_format($expenses,0) . "</td>";
         }
 	echo "<td align=\"right\"><b>" . number_format($total,2) . "</b></td></tr>";
+	$data[$counter][3]= number_format($total   ,0,".","");
+	$counter++;
 }
 echo "</table>";
 
@@ -114,5 +123,33 @@ function getmonthly($yyyy, $mm) {
 
 $mysqli->close();
 ?>
+<div id="curve_chart" style="width: 1800px; height: 1000px"></div>
+<pre>
 
+<pre>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+	['Year', 'Revenue', 'Expenses', 'Profit']
+<?php
+	for ($i=1;$i<$counter;$i++) {
+		echo ",\n [ " . $data[$i][1] . "," . $data[$i][2]  . "," . $data[$i][3]  . "," . $data[$i][4]  . "]"; 
+	}
+?>  ]);
+
+        var options = {
+          title: 'Performance',
+          curveType: 'function',
+          legend: { position: 'top_right' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 
